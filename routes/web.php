c1cbbada568dc8594c4693f0dcd501c892cf3a18
jarji <100996174+jarjia@users.noreply.github.com>
2024-04-16 23:30:54 +0400

    feat: set up routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['controller' => AdminController::class], function () {
+    Route::get('/', 'index')->name('home.page');
+    Route::get('/admin', 'indexCategories')->name('admin.page');
+    Route::get('/product/{product}', 'show')->name('product.details.page');
+});
+
+Route::group(['controller' => ProductsController::class, 'prefix' => '/api'], function () {
+    Route::post('/product', 'create')->name('create.product');
+    Route::delete('/product/{product}', 'destroy')->name('destroy.product');
+});
+
+Route::group(['controller' => CategoriesController::class, 'prefix' => '/api'], function () {
+    Route::post('/category', 'create')->name('create.category');
 });
