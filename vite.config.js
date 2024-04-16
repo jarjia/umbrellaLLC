fb1d367b2a3b5e6d6e818c5cb3d50104ae740ba0
jarji <100996174+jarjia@users.noreply.github.com>
2024-04-16 23:31:24 +0400

    feat: other

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import 'dotenv/config';
 
 export default defineConfig({
-    build: {
-        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
-        cssMinify: process.env.APP_ENV === 'production',
-    },
     plugins: [
+        react(),
         laravel({
-            input: ['resources/react/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
-        react(),
     ],
 });
